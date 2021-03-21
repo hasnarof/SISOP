4546083a91ddcdb30fb2337f8e187896dbdfc2f9
hasnarof <hasnarof@gmail.com>
2021-03-21 16:30:15 +0700

    asistensi

--- modul-1/no3.sh
+++ modul-1/no3.sh
@@ -9,12 +9,14 @@
 # -- Iris Versicolour
 # -- Iris Virginica
 
-awk '/Iris-setosa/ {
-        split($0, idx, ",");
-        if (strtonum(idx[1]) > 5.4) {
-                # printf "sepal length : %.1f petal area : %.2f\n", strtonum(idx[1]), 
-                #         strtonum(idx[3]) * strtonum(idx[4]);
-                printf "Luas petal pada baris %d adalah %.2f\n", NR, strtonum(idx[3]) * strtonum(idx[4]);
-        }
+# awk -F "," '/Iris-setosa/ {
+#         # split($0, idx, ",");
+#         if (strtonum(idx[1]) > 5.4) {
+#                 # printf "sepal length : %.1f petal area : %.2f\n", strtonum(idx[1]), 
+#                 #         strtonum(idx[3]) * strtonum(idx[4]);
+#                 printf "Luas petal pada baris %d adalah %.2f\n", NR, strtonum(idx[3]) * strtonum(idx[4]);
+#         }
  
-}' iris.data
+# }' iris.data
+
+awk -F "," 'BEGIN {n=0} $5 ~ /Iris-setosa/ {++n; luas=$3*$4; if ($1 > 5.4) printf "Luas petal pada baris %d adalah %.2f\n", n, luas}' iris.data
